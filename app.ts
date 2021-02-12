c514f8dfe01cec72f624909ec7cae173d3e47e49
Zaharie Cont <zahariecont@Hotmail.com>
2021-02-12 12:20:19 -0300

    fixed exist file session.json

--- app.ts
+++ app.ts
@@ -2,8 +2,8 @@ import server from "./config/server";
 import { initBot } from "./ClientModule";
 
 var port = process.env.VCAP_APP_PORT || 3000;
-initBot();
-server.listen("8080", ()=>{
+server.listen("8080", () => {
+  initBot();
   console.log("running");
 });
 process.on("uncaughtException", function (err) {
