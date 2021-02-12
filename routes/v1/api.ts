3f481c33c95a684ea069a91b35c6c11228155016
Zaharie Cont <zahariecont@Hotmail.com>
2021-02-12 11:49:09 -0300

    fixed parameters qrcode

--- routes/v1/api.ts
+++ routes/v1/api.ts
@@ -3,7 +3,7 @@ import MessageController from "../../controllers/MessageController";
 export default function (app) {
   const botController = new BotController();
   const messageController = new MessageController();
-  app.get("/api/qrcode/:phone", botController.getQrCode);
+  app.get("/api/qrcode", botController.getQrCode);
   app.post("/api/message/:phone", messageController.sendMessage);
   app.post("/api/location/:phone", messageController.sendLocation);
   app.post("/api/media/:phone", messageController.sendMedia);
