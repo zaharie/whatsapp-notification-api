07b1b5cbb3d0c6e73d221dbd382d858f6ef49670
Zaharie Cont <zahariecont@Hotmail.com>
2021-02-12 11:33:25 -0300

    base64 logic

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
 import server from "./config/server";
 import { initBot } from "./ClientModule";
-var host = process.env.VCAP_APP_HOST || "localhost";
+
 var port = process.env.VCAP_APP_PORT || 3000;
 initBot();
-server.listen(8080, host);
+server.listen("0.0.0.0", ()=>{
+  console.log("running");
+});
 process.on("uncaughtException", function (err) {
   console.log(err);
 });
