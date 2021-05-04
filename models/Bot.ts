bad82cf3e0930910e79fafa14a51c2ed8720ac9f
Zaharie Cont <zahariecont@Hotmail.com>
2021-05-04 18:34:56 -0300

    removido mensagem

--- models/Bot.ts
+++ models/Bot.ts
@@ -59,12 +59,7 @@ class Bot {
     });
 
     client.on("message", (msg) => {
-      let from = msg.from.split("-");
-      if (from.length == 1) {
-        msg.reply(
-          "Oi, eu sou um sistema de notificação de pede logo, por favor para qualquer outro assunto entre em contato com o estabelecimento, Obrigado !"
-        );
-      }
+ 
     });
     return;
   };
