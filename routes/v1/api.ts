1c5c6509dd932938566c29809b240391a4f08f62
Zaharie Cont <zahariecont@Hotmail.com>
2021-05-04 09:08:11 -0300

    adicionado endpoint de status

--- routes/v1/api.ts
+++ routes/v1/api.ts
@@ -4,6 +4,9 @@ export default function (app) {
   const botController = new BotController();
   const messageController = new MessageController();
   app.get("/api/qrcode", botController.getQrCode);
+  app.get("/status", (req, res) => {
+    return res.status(200).send({ status: "ok" });
+  });
   app.post("/api/message/:phone", messageController.sendMessage);
   app.post("/api/location/:phone", messageController.sendLocation);
   app.post("/api/media/:phone", messageController.sendMedia);
